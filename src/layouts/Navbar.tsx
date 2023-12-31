1564daae218cf1bf6d2c0cf2d1faf01172688a79
sumoncse19 <mdsumoncse19@gmail.com>
2023-12-31 15:25:34 +0600

    header redesign

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -12,29 +12,31 @@ const Navbar = () => {
     dispatch(resetUser());
     navigate("/auth/#login");
   };
-
+  // linear-gradient(#141e30, #243b55)
   return (
-    <div className="flex justify-between p-5">
-      <div>Book Guru</div>
-      <ul className="flex items-center">
-        <li>
-          <button>
-            <Link to="/">Home</Link>
-          </button>
-        </li>
-        <li>
-          <button>
-            <Link to="/all-books">All Books</Link>
-          </button>
-        </li>
-        <li>
-          <button>
-            <Link to="/add-new-book">Add New</Link>
-          </button>
-        </li>
-      </ul>
-      <div className="cursor-pointer" onClick={handleLogout}>
-        {user.email ? "Log Out" : "Log In"}
+    <div className="flex justify-between p-5 bg-[#141e30] text-white">
+      <div className="font-bold">Book Guru</div>
+      <div className="flex space-x-2">
+        <ul className="flex items-end  space-x-2">
+          <li>
+            <button>
+              <Link to="/">Home</Link>
+            </button>
+          </li>
+          <li>
+            <button>
+              <Link to="/all-books">All Books</Link>
+            </button>
+          </li>
+          <li>
+            <button>
+              <Link to="/add-new-book">Add New</Link>
+            </button>
+          </li>
+        </ul>
+        <div className="cursor-pointer" onClick={handleLogout}>
+          {user.email ? "Log Out" : "Log In"}
+        </div>
       </div>
     </div>
   );
